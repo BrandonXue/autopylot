b4ec78f618fed35f2a11c75794bf84144fe260a3
Jacob Rapmund <jwrapmund@gmail.com>
2020-10-25 10:57:46 -0700

    Basic movement.

--- SimpleCarGame.py
+++ SimpleCarGame.py
@@ -1,26 +1,46 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 1080, 720
 speed = [2,2]
 black = 0, 0, 0
 blue = 0, 0, 255
+
+keys = [False]*1024
+
+vel = 1, 1
 
 screen = pygame.display.set_mode(size)
 
 testRect = pygame.Rect(0, 0, 10, 10)
 
 while True:
+    #update events
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         
-        #game logic
+        if event.type == pygame.KEYDOWN:
+            keys[event.key] = True
         
-        #screen reset
-        screen.fill(black)
+        if event.type == pygame.KEYUP:
+            keys[event.key] = False
         
-        #render
-        pygame.draw.rect(screen, blue, testRect)
+    #game logic
+    if keys[pygame.K_w]:
+        testRect = testRect.move(0, -1)
+    if keys[pygame.K_s]:
+        testRect = testRect.move(0, 1)
+    if keys[pygame.K_a]:
+        testRect = testRect.move(-1, 0)
+    if keys[pygame.K_d]:
+        testRect = testRect.move(1, 0)
         
-        #swap buffers
-        pygame.display.flip()
+    
+    #screen reset
+    screen.fill(black)
+    
+    #render
+    pygame.draw.rect(screen, blue, testRect)
+    
+    #swap buffers
+    pygame.display.flip()
