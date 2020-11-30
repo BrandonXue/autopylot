c68b35a4d9eb35eb7cd6257fdc754859d891c0e3
Jacob Rapmund <jwrapmund@gmail.com>
2020-11-30 12:12:42 -0800

    commit for pull.

--- data_processing.py
+++ data_processing.py
@@ -7,9 +7,9 @@ import cv2
 import dill
 import mss
 import numpy as np
-# import tensorflow as tf
 
-# from tensorflow import keras
+import tensorflow as tf
+from tensorflow import keras
 
 class DataProcessor:
     def __init__(self, pipe_conn, exit_signal):
@@ -20,24 +20,21 @@ class DataProcessor:
         self.pipe_conn.close()
 
     def start(self):
-    
-        #with mss.mss() as sct:
-        #    monitor = sct.monitors[1]
-        #    pixels = np.array(sct.grab(monitor))
-        #    pixels = cv2.cvtColor(pixels, cv2.COLOR_RGBA2GRAY)
-        #    pixels = cv2.resize(pixels, (GRAYSCALE_DIM, GRAYSCALE_DIM))
-        #    pixels = pixels.astype('float64')
             
         sct = mss.mss()
         mon = sct.monitors[1]
         
         monitor = {
-            "top": mon["top"],  # 100px from the top
-            "left": mon["left"],  # 100px from the left
+            "top": mon["top"],
+            "left": mon["left"],
             "width": VIEWPORT_WIDTH,
             "height": VIEWPORT_HEIGHT,
             "mon": 1,
         }
+        
+        dense = keras.layers.Dense(units=16)
+        inputs = keras.Input(shape=(GRAYSCALE_DIM, GRAYSCALE_DIM, 3))
+        
             
         while True:
             if self.pipe_conn.poll():
@@ -46,13 +43,13 @@ class DataProcessor:
                     break
                 
             # TODO: Train neural net here
-            pixels = np.array(sct.grab(monitor))
+            pixels = np.array(sct.grab(monitor)).astype('float32')
             pixels = cv2.cvtColor(pixels, cv2.COLOR_RGBA2GRAY)
-            pixels = cv2.resize(pixels, (GRAYSCALE_DIM, GRAYSCALE_DIM))
+            pixels = cv2.resize(pixels, (GRAYSCALE_DIM, GRAYSCALE_DIM)) / 255
             #pixels = np.transpose(pixels.astype('float64'), (1,0,2))
-            pixels = np.transpose(pixels.astype('float64'), (1,0))
+            pixels = np.transpose(pixels, (1,0))
             
-            print(pixels.shape)
+            
             
             if dill.pickles(pixels):
                 self.pipe_conn.send(pixels)
